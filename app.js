854befb62b18411152b355fc1e16fa48ae76e566
Han Jie Lee <hjl386@nyu.edu>
2017-04-06 12:20:39 -0400

    Working on register, reference errors

--- app.js
+++ app.js
@@ -7,25 +7,25 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 require('./db');
 
 const [PORT, HOST] = [3000, '127.0.0.1'];
 const app = express();
 const User = mongoose.model('User');
+const saltRounds = 10;	// Might move to app.post for register
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
-//app.use(session(sessionOptions));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-/*
 const sessionOptions = {
 	secret: 'secret thing',
 	resave: true, 
 	saveUninitialized: true
 }
-*/
+app.use(session(sessionOptions));
 
 app.get('/css/base.css', (req, res) => {
 	res.render('base.css');
@@ -44,15 +44,74 @@ app.get('/register', (req, res) => {
 	});
 });
 
-app.post('/register', (req, res) => {
-	User.findOneAndUpdate({$push: {username: req.body.username, password: req.body.password}}, (err) => {
-		if (err) {
+function regenerateSession(err, req, res){
+	if(!err){
+		req.session.username = uHashed.username;
+		res.redirect('/');
+	} else{
+		console.log(err)
+		res.send("ERROR");
+	}	
+}
+
+function hashP(err, req, res, hash){
+	if(err){
+		console.log(err);
+	}
+	const uHashed = new User({
+		username: req.body.username,
+		password: hash
+	});
+	uHashed.save(err =>{
+		if(err){
 			console.log(err);
 		}
-		if (req.body.password < 8){
-			
+		req.session.regenerate(regenerateSession);
+	});
+}
+
+app.post('/register', (req, res) => {
+	const u = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+	User.findOne({username: req.body.username}, (err, user) => {
+		if(user){
+			console.log("DOES IT RE RENDER THE REGISTRATION PAGE?");
+			res.render('register', {userExists: true});
 		}
 	});
+	if(u.password.length < 8){
+		res.render('register', {passwordLength: true});
+	} else{
+		bcrypt.hash(u.password, saltRounds, hashP()); 
+	}
+/*	} else{
+		bcrypt.hash(u.password, saltRounds, (err, hash) => { 
+			if(err){
+				console.log(err);
+			}
+			const uHashed = new User({
+				username: req.body.username,
+				password: hash
+			});
+			uHashed.save(err => {
+				if(err){
+					console.log(err);
+				}
+				req.session.regenerate(err => {
+					if(!err){
+						req.session.username = uHashed.username;
+						res.redirect('/');
+					} else {
+						console.log(err);
+						res.send('An error has occured, please see the server logs for more information');
+					}
+				});		
+			});
+		});
+	}
+*/
 });
 
 app.listen(PORT, HOST);
--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,17 @@
 // A module of the callback functions 
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+const mongoose = require('mongoose');
+const User = mongoose.model('User'); 
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+	User.findOneAndUpdate({}, {$push: {username: req.body.username, password: req.body.password}}, (err) => {
+		if(err){
+			console.log(err);
+		}
+		
+	});	
+});
